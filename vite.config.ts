138886d80fda97b484769ff1610de76fbfd91b10
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2026-07-07 19:59:36 +0600

    vite-config-edit

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
+
   server: {
     port: 3000,
     open: true,
   },
-})
+
+  preview: {
+    host: "0.0.0.0",
+    port: 9008,
+    allowedHosts: ["erp.softypy.com"],
+  },
+});
